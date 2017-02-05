af7db5b84cf00d0f784346cefe96f47f9316daea
Matt Boyer <mboyer@sdf.org>
2017-02-05 21:10:57 +0000

    Needs more tests!

--- src/metadata/linker.py
+++ src/metadata/linker.py
@@ -2,7 +2,7 @@ from ..metadata.assigned import AssignedSubnet
 from ..tools.logger import ModuleLogger
 
 from functools import reduce
-from sqlalchemy import func
+from sqlalchemy import func, null
 
 log = ModuleLogger(__name__)
 
@@ -67,3 +67,29 @@ class SubnetLinker(object):
             start = sequence[0].floor()
             end = sequence[-1].ceiling()
             log.info("%d contiguous subs: %s - %s", count, start, end)
+
+        # Let's inspect the subnets that have neither a next nor a prev, ie.
+        # those subnets that weren't the smallest for their network address
+        container_subnet_iter = self.data_mgr.query(
+            AssignedSubnet,
+        ).filter(
+            AssignedSubnet.next_subnet_id == null(),
+            AssignedSubnet.previous_subnet_id == null(),
+        )
+
+        child_subnets = []
+        for cs in container_subnet_iter:
+            log.info("Finding subnets nested under %r", cs)
+            children_subnet_iter = self.data_mgr.query(
+                AssignedSubnet
+            ).filter(
+                AssignedSubnet._network >= cs.floor(),
+                AssignedSubnet._network <= cs.ceiling(),
+                AssignedSubnet._prefix_length > cs.prefix_length
+            )
+            for nested_subnet in children_subnet_iter:
+                log.info("Found %r", nested_subnet)
+                nested_subnet.parent = cs
+                child_subnets.append(nested_subnet)
+
+        self.data_mgr.update_records(child_subnets)
