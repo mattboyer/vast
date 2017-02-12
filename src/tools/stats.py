3bc6a8f22f270972db0969ce24014111ec7ccdb9
Matt Boyer <mboyer@sdf.org>
2017-02-12 17:34:54 +0000

    ....

--- src/tools/stats.py
+++ src/tools/stats.py
@@ -1,8 +1,12 @@
 from . import Command, CLI_subcmd
 from ..metadata.orm import DataManager
+from ..metadata.assigned import AssignedSubnet
 from ..tools.logger import ModuleLogger
 
 from collections import defaultdict
+from functools import reduce
+
+from sqlalchemy import func
 
 log = ModuleLogger(__name__)
 
@@ -20,6 +24,28 @@ class StatsCmd(Command):
         self.data_mgr = DataManager()
         self._print_stats()
 
+    @staticmethod
+    def enumerate_contiguous_subnets(contiguous_subnets, next_subnet_up):
+        # contiguous_subnets is a list of 2-tuples, with each comprising a
+        # count of contiguous AssignedSubnet objects and the corresponding list
+        # of contiguous AssignedSubnet instances
+
+        # If we have an empty list, then this is easy enough
+        if not contiguous_subnets:
+            contiguous_subnets.append([next_subnet_up])
+            return contiguous_subnets
+
+        # We have a non-empty list
+        contiguous_list = contiguous_subnets[-1]
+        if int(next_subnet_up.floor()) == \
+                1 + int(contiguous_list[-1].ceiling()):
+
+            contiguous_list.append(next_subnet_up)
+            contiguous_subnets[-1] = contiguous_list
+        else:
+            contiguous_subnets.append([next_subnet_up])
+        return contiguous_subnets
+
     def _print_stats(self):
         prefix_lengths = defaultdict(int)
         all_subs = self.data_mgr.all_records()
@@ -33,3 +59,23 @@ class StatsCmd(Command):
         length_distribution = sorted(prefix_lengths.keys(), reverse=True)
         for length in length_distribution:
             log.info("/%d count:	%d", length, prefix_lengths[length])
+
+        assigned_subnet_iter = self.data_mgr.query(
+            AssignedSubnet,
+        ).having(
+            func.max(AssignedSubnet.mapped_prefix_length)
+        ).group_by(
+            AssignedSubnet.mapped_network
+        )
+
+        contiguous_coverage = reduce(
+            self.enumerate_contiguous_subnets,
+            assigned_subnet_iter,
+            []
+        )
+
+        for sub in contiguous_coverage:
+            contig_start = sub[0].floor()
+            contig_end = sub[-1].ceiling()
+            contig_length = sum(len(s) for s in sub)
+            log.info("%r → %r: %r", contig_start, contig_end, contig_length)
