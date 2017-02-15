9fdf5590af6cdd2ed050fa5eca27a93f78d3a484
Matt Boyer <mboyer@sdf.org>
2017-02-15 22:07:30 +0000

    Separation of concerns

---
+++ src/metadata/stats.py
@@ -0,0 +1,43 @@
+from ..metadata.orm import DataManager
+from ..net.IPv4 import Address
+
+from collections import defaultdict
+from decimal import Decimal
+
+from ..tools.logger import ModuleLogger
+
+log = ModuleLogger(__name__)
+
+
+class StatsManager(object):
+
+    def __init__(self):
+        self.data_mgr = DataManager()
+
+    def distribution(self):
+        prefix_lengths = defaultdict(int)
+        subnet_count = 0
+
+        for sub in self.data_mgr.all_records():
+            prefix_lengths[sub.prefix_length] += 1
+            subnet_count += 1
+
+        return (subnet_count, prefix_lengths)
+
+    def coverage(self):
+        contiguous_coverage = self.data_mgr.group_contiguous_subnets()
+
+        whole_unicast_address_space = int(Address("225.255.255.255")) + 1
+        total_unicast_coverage = 0
+
+        for contig_block in contiguous_coverage:
+            contig_start = contig_block[0].floor()
+            contig_end = contig_block[-1].ceiling()
+            contig_length = sum(len(subnet) for subnet in contig_block)
+            total_unicast_coverage += contig_length
+
+            log.debug("%r → %r: %r", contig_start, contig_end, contig_length)
+
+        coverage = round(100 * (Decimal(total_unicast_coverage) /
+                                Decimal(whole_unicast_address_space)), 3)
+        return total_unicast_coverage, coverage
